612cd6f88b2e64e46bc92a0734ef9a947000e018
Imronshoh <monikapi2022@gmail.com>
2023-11-27 22:47:36 +0500

    d

---
+++ js/main.js
@@ -0,0 +1,15 @@
+var elInp = document.querySelector(".inp");
+
+function myFn() {
+  var nm = elInp.value;
+
+  if (nm % 3 == 0 && nm % 7 == 0) {
+    console.log("Bu son 3ga va 7ga bolinadi");
+  } else if (nm % 7 == 0) {
+    console.log("Bu son 7ga bolinadi");
+  } else if (nm % 3 == 0) {
+    console.log("Bu son 3ga bolinadi");
+  } else {
+    console.log("Bu son vashe bolinmedi 3ga yoki 7ga");
+  }
+}
