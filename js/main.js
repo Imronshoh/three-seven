7fa51ec483aa7d657f6bd4a0d118f17e05b30a77
Imronshoh <monikapi2022@gmail.com>
2023-11-27 22:58:26 +0500

    add

--- js/main.js
+++ js/main.js
@@ -3,7 +3,9 @@ var elInp = document.querySelector(".inp");
 function myFn() {
   var nm = elInp.value;
 
-  if (nm % 3 == 0 && nm % 7 == 0) {
+  if (nm == "") {
+    alert("Nimadur kiriting iltimos");
+  } else if (nm % 3 == 0 && nm % 7 == 0) {
     console.log("Bu son 3ga va 7ga bolinadi");
   } else if (nm % 7 == 0) {
     console.log("Bu son 7ga bolinadi");
@@ -12,4 +14,5 @@ function myFn() {
   } else {
     console.log("Bu son vashe bolinmedi 3ga yoki 7ga");
   }
+  elInp.value = "";
 }
